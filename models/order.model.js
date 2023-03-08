d02628ae6ec64e131ed69c806b9a3f8084692279
RageousFoxTrot <rageousfoxtrot@gmail.com>
2023-03-08 00:37:23 +0000

    2 files modified

--- models/order.model.js
+++ models/order.model.js
@@ -1,29 +1,23 @@
 import { model as DBModel, Schema as DBSchema } from 'mongoose';
 const $types = DBSchema.Types;
 
+// TODO: Add NIF as optional.
 const OrderSchema = new DBSchema(
   {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    customer: { type: $types.ObjectId, ref: 'customer', required: true },
+    status: { type: String, required: true },
     salt: { type: String, required: true },
-    phone: String,
-    address: [{ type: $types.ObjectId, ref: 'address', require: true }],
     cart: [
       {
         product: { type: $types.ObjectId, ref: 'product', required: true },
         amount: { type: Number, required: true },
       },
     ],
-    orders: [{ type: $types.ObjectId, ref: 'order', required: true }],
-    wishlist: [{ type: $types.ObjectId, ref: 'order', required: true }],
-    vendor: { type: $types.ObjectId, ref: 'vendor', default: null },
   },
   {
     toJSON: {
       transform(doc, $) {
         delete $['__v'];
-        delete $['password'];
-        delete $['salt'];
       },
     },
     timestamps: true,
@@ -35,7 +29,7 @@ OrderSchema.pre('validate', () => {
 });
 
 OrderSchema.pre('save', () => {
-  // If password is included, hash with the salt and override it.
+  // Removes items that the amount is 0.
 });
 
 export default DBModel('order', OrderSchema, 'order');
