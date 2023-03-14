e041473dd99caf695d0ebcc4abf0b34e6a639106
RageousFoxTrot <37639369+RageousFoxTrot@users.noreply.github.com>
2023-03-14 00:30:33 +0000

    2 files modified, 1 added

---
+++ controllers/customer.controller.js
@@ -0,0 +1,19 @@
+class CustomerService {
+  constructor() {}
+
+  async CreateCustomer() {}
+  async CreateAddress() {}
+  async AddItemToCart() {}
+  async AddOrderToProfile() {}
+  async AddProductToWishlist() {}
+  async CreateVendorStore() {}
+
+  async FindCustomer() {}
+  async FindCustomerByID() {}
+
+  async RemoveItemsFromCart() {}
+  async RemoveAddress() {}
+  async RemoveItemsFromWhitelist() {}
+  async DeleteVendor() {}
+  async DeleteUser() {}
+}
---
+++ routers/index.js
@@ -0,0 +1,2 @@
+export * as CustomerRouter from './customer.router.js';
+export * as ProductRouter from './product.router.js';
